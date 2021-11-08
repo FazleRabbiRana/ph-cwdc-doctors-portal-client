4c2722ff116d8f57123d5657bb2207af268601ac
FazleRabbiRana <ranafazrabbi@gmail.com>
2021-11-08 22:07:01 +0600

    remote locale date string

--- src/Pages/Dashboard/Appointments/Appointments.js
+++ src/Pages/Dashboard/Appointments/Appointments.js
@@ -16,7 +16,7 @@ const Appointments = ({ date }) => {
 	const [appointments, setAppointments] = useState([]);
 
 	useEffect(() => {
-		const url = `https://whispering-forest-63163.herokuapp.com/appointments?email=${user.email}&date=${date}`;
+		const url = `https://whispering-forest-63163.herokuapp.com/appointments?email=${user.email}&date=${date.toLocaleDateString()}`;
 		fetch(url, {
 			headers: {
 				'authorization': `Bearer ${token}`,
